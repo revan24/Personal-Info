8c881afa55f8439a2147407e2b24489fc20e9ad7
revan24 <45130871+revan24@users.noreply.github.com>
2020-08-14 20:34:27 -0400

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+var fullName = (`${firstName} ${lastName}`)
+
+document.write(`Your full name is ${fullName}. You are ${age} y/o.`);
+console.log(`Your full name is ${fullName}.`);
+console.log(`You are ${age} y/o.`)
+
+
+
+
+
+
